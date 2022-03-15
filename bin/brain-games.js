ebbb76cc943a4f9cf8bf12fd5c56184e8a781838
Александр Шадрин <shadrinalexander@mail.ru>
2022-03-15 08:54:15 +0300

    /5

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,13 +1,8 @@
 #!/usr/bin/env node
 
 import greating from '../src/cli.js';
-import brain_even from '../bin/brain-even.js';
+
 
 console.log('Welcome to the Brain Games!');
 const name = greating();
 console.log(`Hello, ${name}`);
-let cli_answer_1 = brain_even();
-while (cli_answer_1 != 1) {
-    cli_answer_1 = brain_even();
-} 
-console.log(`Congratulations, ${name}!`);
--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 import readlineSync from 'readline-sync';
 import greating from '../src/cli.js';
 
