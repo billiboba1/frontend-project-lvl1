f22a92ee6d3e22ce6e6840897b3dc465a977237a
Александр Шадрин <shadrinalexander@mail.ru>
2022-03-15 08:57:36 +0300

    /6

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,7 @@
 import readlineSync from 'readline-sync';
 
 const greating = () => {
-    const name = readlineSync.question('May you have a name? ');
+    const name = readlineSync.question('May I have your name? ');
     return (name);
 }
 
