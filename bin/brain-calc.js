579e8eedfc46f6ea4abc8af5d05fd70562d8836b
Александр Шадрин <shadrinalexander@mail.ru>
2022-03-27 09:47:43 +0300

    /18

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -17,10 +17,9 @@ const brainCalc = () => {
   do {
     const question = getRandom(2, 99);
     let rightAnswer,
-        firstNum,
-        secondNum,
-        cliAnswer;
-        
+      firstNum,
+      secondNum,
+      cliAnswer;
     switch (question) {
       case 0:
         firstNum = getRandom(100);
