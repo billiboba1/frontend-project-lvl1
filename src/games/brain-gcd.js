ea449ef9028da7e9bb6c8d992d2ce66fa3a1fef4
Александр Шадрин <shadrinalexander@mail.ru>
2022-04-17 06:36:37 +0300

    //

--- src/games/brain-gcd.js
+++ src/games/brain-gcd.js
@@ -2,22 +2,23 @@ import startMainLogic from '../index.js';
 import getRandom from '../utils.js';
 
 const getGcd = (firstNumber, secondNumber, rightAnswer) => {
-  const forWhile = true;
-  while (forWhile) {
+  minimalNumber = rightAnswer;
+  while (true) {
     if ((firstNumber % rightAnswer === 0) && (secondNumber % rightAnswer === 0)) {
-      return rightAnswer;
+      break;
     }
-    rightAnswer -= 1;
+    minimalNumber -= 1;
   }
-  return 1;
+  return minimalNumber;
 };
 
 const giveBrainGcdLogic = () => {
   const giveNeedingParameters = {};
   const secondNumber = getRandom(2, 60);
   const firstNumber = getRandom(2, 60);
+  const minimalNumber = Math.min(firstNumber, secondNumber);
   giveNeedingParameters.question = `Question: ${firstNumber} ${secondNumber}\nYour answer: `;
-  giveNeedingParameters.rightAnswer = getGcd(firstNumber, secondNumber, Math.min(secondNumber, firstNumber));
+  giveNeedingParameters.rightAnswer = getGcd(firstNumber, secondNumber, minimalNumber);
   return giveNeedingParameters;
 };
 
